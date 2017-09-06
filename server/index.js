ecc41ab8950573e31d6928237fea3a57be179a3b
lionzxy <nikita@kulikof.ru>
2017-09-07 01:01:02 +0300

    Add base nodejs server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.send('404')
+});
+
+app.listen(port);
