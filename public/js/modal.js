fb59f32591aceabd636c857b3f77b924129381e5
Ansile <twoflouers@yandex.ru>
2017-09-21 12:08:57 +0300

    Reworking to template-based approach, minor styling changes

--- public/js/modal.js
+++ public/js/modal.js
@@ -15,6 +15,7 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = 'none';
+    modal.focus();
 };
 
 // When the user clicks anywhere outside of the modal, close it
