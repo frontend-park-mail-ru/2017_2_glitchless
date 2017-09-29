ddba779978c2b0c8d1fc54b627c622b6a26e9adb
Unknown <twoflouers@yandex.ru>
2017-09-29 18:45:15 +0300

    Added docs to EventBus, minor refactoring

--- src/utils/EventBus.js
+++ src/utils/EventBus.js
@@ -1,14 +1,31 @@
 class EventBus {
+    
+    /**
+     * @return {EventBus} EventBus object with empty event list
+     */
     constructor() {
         this.events = {};
     }
 
+    /**
+     * Executes all callbacks tied to the event
+     *
+     * @param {String} key Name of the event
+     * @param {Object} args Argument with which the callbacks will be executed
+     */
     emitEvent(key, args) {
         this.events[key].forEach((callback) =>
             callback(args)
         );
     }
 
+    /**
+     * Adds a callback to the event
+     *
+     * @param {String} key Name of the event
+     * @param {Function} callback Function that's going to be executed on emmitting event
+     * @return {Function} Function that unsubscribes the callback from event 
+     */
     subscribeOn(key, callback) {
         if(this.events[key] == undefined){
             this.events[key] = [];
@@ -19,6 +36,12 @@ class EventBus {
         };
     }
 
+    /**
+     * Removes a callback from the event
+     *
+     * @param {String} key Name of the event
+     * @param {Function} callback Function that's executed on emmitting event
+     */
     subscribeOff(key, callback) {
         this.events[key] = this.events[key].filter((it) => {
             it != callback;
