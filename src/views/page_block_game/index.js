f99e5488a2792b2169300c41866b46271533919e
Ansile <twoflouers@yandex.ru>
2017-10-21 23:09:20 +0300

    Reworked game to run in fullscreen, added central element

--- src/views/page_block_game/index.js
+++ src/views/page_block_game/index.js
@@ -1,19 +1,21 @@
 function init(serviceLocator) {
     let gameField = document.getElementById('game');
     const PIXI = require('pixi.js');
-    console.log(PIXI)
-    let app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+    console.log(PIXI);
+    let app = new PIXI.Application(window.innerWidth, window.innerHeight * 0.89, { backgroundColor: 0x1099bb });
     gameField.appendChild(app.view);
+    console.log(window.innerWidth, window.innerHeight);
+// Scale mode for all textures, will not retain pixelation
+    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.LINEAR;
 
-// Scale mode for all textures, will retain pixelation
-    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-
-    let sprite = PIXI.Sprite.fromImage('./images/glitchless.jpg');
+    let sprite = PIXI.Sprite.fromImage('./images/spacestation.png');
 
 // Set the initial position
     sprite.anchor.set(0.5);
     sprite.x = app.renderer.width / 2;
     sprite.y = app.renderer.height / 2;
+
+    sprite.height = sprite.width = app.renderer.width / 10;
 
 // Opt-in to interactivity
     sprite.interactive = true;
