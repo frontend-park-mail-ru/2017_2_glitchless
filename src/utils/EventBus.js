2fc0d698ef0f435254a362805b77cb69a5f172c3
Ansile <twoflouers@yandex.ru>
2017-10-25 00:19:36 +0300

    New EventBus - perfection made better

--- src/utils/EventBus.js
+++ src/utils/EventBus.js
@@ -13,12 +13,27 @@ class EventBus {
      * @param {String} key Name of the event
      * @param {Object} args Argument with which the callbacks will be executed
      */
-    emitEvent(key, args) {
+    emitEvent(key, args=null) {
         this.events[key].forEach((callback) => {
             console.log('Test');
-            callback(args);
+            if (args)
+                callback(args);
+            else
+                callback();
         });
     }
+
+    /**
+     * tied to the event
+     *
+     * @param {Function} callback Callback to wrap
+     * @param {Object} context Context in which callback will be executed
+     * @param {Iterable} args Arguments with which the callback will be executed
+     */
+    proxy(callback, context=this, args=[]) {
+        return callback.bind(context, ...args);
+    }
+
 
     /**
      * Adds a callback to the event
@@ -27,11 +42,11 @@ class EventBus {
      * @param {Function} callback Function that's going to be executed on emmitting event
      * @return {Function} Function that unsubscribes the callback from event
      */
-    subscribeOn(key, callback) {
+    subscribeOn(key, callback, context=this) {
         if (this.events[key] == undefined) {
             this.events[key] = [];
         }
-        this.events[key].push(callback);
+        this.events[key].push(callback.bind(context));
         return () => {
             this.subscribeOff(key, callback);
         };
