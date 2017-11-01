9e00b62b89045b83471a67cf6bd8e02d6b6419e8
Ansile <twoflouers@yandex.ru>
2017-11-01 15:22:26 +0300

    Added docs, minor changes

--- src/game/physics/PhysicPrimitives.js
+++ src/game/physics/PhysicPrimitives.js
@@ -12,19 +12,24 @@ class Circle {
      * @param {Number} radius Circle radius
      * @param {Point} center Circle center
      *
-     * @return {Line} A-normalized equation of @param line
+     * @return {Circle}
      */
     constructor(radius, center=new Point(0,0)) {
         this.R = radius;
         this.center = center;
     }
 
+    /**
+     * @param {Point} point
+     *
+     * @return {Point} point position relative to the center of circle
+     */
     centrate(point) {
         return new Point(point.x - this.center.x, point.y - this.center.y);
     }
 
     /**
-     * @link http://algolist.manual.ru/maths/geom/equation/circle.php
+     * @see {@link http://algolist.manual.ru/maths/geom/equation/circle.php}
      *
      * @param {Point} point1
      * @param {Point} point2
@@ -58,43 +63,89 @@ class Circle {
 }
 
 class Arc extends Circle {
+    /**
+     * @param {Number} radius Radius of the circle, on which arc is located
+     * @param {Point[]} boundingPoints Two points (number 0, 1 in array) that limit the arc on the circle,
+     *      one point (number 3 in array) that is located between those points and decides in which direction
+     *      of those 2 points arc is located
+     * @todo consider reworking point 3 to direction (clockwise/counter-clockwise)
+     * @param {Point} center Circle center
+     *
+     * @return {Arc}
+     */
     constructor(radius, boundingPoints, center=new Point(0,0)) {
         super(radius, center);
         this.bound1 = boundingPoints[0];
         this.bound2 = boundingPoints[1];
+        this.midPoint = boundingPoints[2];
 
         //relative coordinates
-        const bound1X = this.bound1.x - center.x;
-        const bound1Y = this.bound1.y - center.y;
-        const bound2X = this.bound2.x - center.x;
-        const bound2Y = this.bound2.y - center.y;
+        const relBound1 = this.centrate(this.bound1);
+        const relBound2 = this.centrate(this.bound2);
+        const relMidPoint = this.centrate(this.midPoint);
 
-        const angleBound1 = Math.atan2(bound1Y, bound1X);
-        const angleBound2 = Math.atan2(bound2Y, bound2X);
+        const angleBound1 = Math.atan2(relBound1.y, relBound1.x);
+        const angleBound2 = Math.atan2(relBound2.y, relBound2.x);
+        this.angleMidPoint = Math.atan2(relMidPoint.y, relMidPoint.x);
 
         this.angleBound1 = Math.min(angleBound1, angleBound2);
         this.angleBound2 = Math.max(angleBound1, angleBound2);
+
+        this.reversed = this.angleMidPoint > angleBound1 && this.angleMidPoint < this.angleBound2;
     }
 
-    static fromPoints(point1, point2, point3) {
-        const circle = super.fromPoints(point1, point2, point3);
-        const arc = Arc.fromCircle(circle, [point1, point2]);
-        return arc;
+    /**
+     * @param {Point} boundingPoint1
+     * @param {Point} boundingPoint2
+     * @param {Point} midPoint
+     *
+     * For details on parameters see {@link Arc#constructor}
+     *
+     * @return {Arc}
+     */
+    static fromPoints(boundingPoint1, boundingPoint2, midPoint) {
+        const circle = super.fromPoints(boundingPoint1, boundingPoint2, midPoint);
+        return Arc.fromCircle(circle, [boundingPoint1, boundingPoint2, midPoint]);
     }
 
+    /**
+     * @param {Circle} circle
+     * @param {Point[]} boundingPoints
+     *
+     * For details on parameters see {@link Arc#constructor}
+     *
+     * @return {Arc}
+     */
     static fromCircle(circle, boundingPoints) {
         return new Arc(circle.R, boundingPoints, circle.center);
     }
 
+    /**
+     * @param {Point} point
+     *
+     * @return {Boolean}
+     */
     contains(point) {
         const relPoint = this.centrate(point);
         const anglePoint = Math.atan2(relPoint.y, relPoint.x);
         console.log('agpoint' + anglePoint);
-        return this.angleBound1 < anglePoint && this.angleBound2 > anglePoint;
+        if (!this.reversed) {
+            return this.angleBound1 < anglePoint && this.angleBound2 > anglePoint;
+        }
+        return this.angleBound2 < anglePoint || this.angleBound1 > anglePoint;
     }
 }
 
 class Line {
+    /**
+     * @param {Number} A Coefficient A of line equation
+     * @param {Number} B Coefficient B of line equation
+     * @param {Number} C Coefficient C of line equation
+     *
+     * @param {Point[]} bounds Bounds of line segment
+     *
+     * @return {Line}
+     */
     constructor(A, B, C=0, bounds=[]) {
         this.A = A;
         this.B = B;
@@ -105,16 +156,31 @@ class Line {
         }
     }
 
-    static createVertical(C, bounds) {
+    /**
+     * @param {Number} C Coefficient C of equation x=C
+     * @param {Point[]} bounds Bounds of line segment
+     *
+     * @return {Line}
+     */
+    static createVertical(C, bounds=[]) {
         const line = new Line(0, 0, C, bounds);
         line.vertical = true;
         return line;
     }
 
+    /**
+    * @return {Boolean}
+    */
     isVertical() {
         return this.vertical;
     }
 
+    /**
+    * @param {Point} point
+    *
+    * @return {Boolean | undefined} True if point is located inside segment, defined by bounds,
+    *   false if it is not, undefined if bounds are not defined
+    */
     segmentContains(point) {
         if (!this.bounds) {
             return;
@@ -145,6 +211,9 @@ class Line {
         return new Line(1, line.B / line.A, line.C / line.A);
     }
 
+    /**
+     * @param {Point[]} bounds Bounds of line segment
+     */
     setBounds(bounds) {
         this.bounds = bounds;
 
@@ -173,9 +242,12 @@ class Line {
         return new Line(A, B, C, [point1, point2]);
     }
 
-    getRadian() {
+    /**
+     * @return {Number} Radian representation (from -PI to +PI) of line's slope
+     */
+    getSlope() {
         if (this.vertical) {
-            return Math.PI / 2;
+            return 0;
         }
         return Math.atan2(-this.A, this.B);
     }
@@ -233,7 +305,7 @@ function findIntersection(line, circle) {
  * @param {Point[]} lineSegmentPoints 2 points defining a line segment
  * @param {Arc} arc
  *
- * @return {Point[] | Boolean} Point of collision, if it exists, else false
+ * @return {[Point[], Number] | Boolean} Point of collision and new line angle if collision exists, else false
  */
 function checkCollision(lineSegmentPoints, arc) {
     const line = Line.fromPoints(...lineSegmentPoints);
@@ -257,12 +329,12 @@ function checkCollision(lineSegmentPoints, arc) {
     if (!collision) {
         return false;
     }
-    console.log(line.getRadian());
+    console.log(line.getSlope());
 
     const relativeCollision = arc.centrate(collision);
     const angle = Math.atan2(relativeCollision.y, relativeCollision.x);
 
-    const reflectionAngle = angle + (angle - line.getRadian());//TODO: check whether this is right
+    const reflectionAngle = angle + (angle - line.getSlope());//TODO: check whether this is right
     return [collision, reflectionAngle];
 }
 
@@ -270,7 +342,7 @@ function checkCollision(lineSegmentPoints, arc) {
 // let points2 = [new Point(1, -2), new Point(1, 2), new Point (3, 0)];
 // const arc = Arc.fromPoints(...points2);
 // const line = Line.fromPoints(...points.slice(0,2));
-
+//
 // console.log(arc);
 // console.log(line);
 // console.log(findIntersection(line, arc));
@@ -280,5 +352,7 @@ module.exports = {
     Point,
     Line,
     Circle,
-    findIntersection
+    Arc,
+    findIntersection,
+    checkCollision
 };
