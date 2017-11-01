0fd1a4cdbaa03dade713de71d144a30eced445be
Ansile <twoflouers@yandex.ru>
2017-11-01 16:29:31 +0300

    Ribs fix

--- src/game/physics/PhysicPrimitives.js
+++ src/game/physics/PhysicPrimitives.js
@@ -138,6 +138,8 @@ class Arc extends Circle {
 
 class Line {
     /**
+     * Creates a line defined by equation Ax + By + C  = 0, where A, B, C are constants, x, y - coordinate variables
+     *
      * @param {Number} A Coefficient A of line equation
      * @param {Number} B Coefficient B of line equation
      * @param {Number} C Coefficient C of line equation
@@ -253,106 +255,10 @@ class Line {
     }
 }
 
-/**
- * @param {Line} line
- * @param {Circle} circle
- *
- * @return {Point[]} Points of intersection
- */
-function findIntersection(line, circle) {
-    const endShift = [circle.center.x, circle.center.y / line.B];
-    const r = circle.R;
-    const EPS = Number.EPSILON;
-
-    if (line.isVertical()) {
-        console.log('vertical');
-        const x0 = line.C;
-        console.log(circle.center.x - line.C);
-        if (circle.R < Math.abs(circle.center.x - line.C)){
-            return [];
-        }
-        if (Math.abs(Math.abs(circle.center.x - line.C) - circle.R) < EPS) {
-            return [new Point(line.C, circle.center.y)];
-        }
-        const tmpSqrt = Math.sqrt(r*r - Math.pow((x0 - circle.center.x), 2));
-        console.log(tmpSqrt);
-        const y1 = circle.center.y + tmpSqrt;
-        const y2 = circle.center.y - tmpSqrt;
-        return [new Point(x0, y1), new Point(x0,y2)];
-    }
-
-    line = Line.normalize(line);
-    const [a, b, c] = [line.A, line.B, line.C - circle.center.y + circle.center.x];
-
-    let x0 = -a * c / (a * a + b * b), y0 = -b * c / (a * a + b * b);
-    if (c * c > r * r * (a * a + b * b) + EPS) {
-        return [];
-    }
-    if (Math.abs(c * c - r * r * (a * a + b * b)) < EPS) {
-        return [x0 + endShift[0], y0 + endShift[1]];
-    }
-    const d = r * r - c * c / (a * a + b * b);
-    const mult = Math.sqrt(d / (a * a + b * b));
-
-    const ax = x0 + b * mult;
-    const bx = x0 - b * mult;
-    const ay = y0 - a * mult;
-    const by = y0 + a * mult;
-    return [new Point(ax + endShift[0], ay + endShift[1]), new Point(bx + endShift[0], by + endShift[1])];
-}
-
-/**
- * @param {Point[]} lineSegmentPoints 2 points defining a line segment
- * @param {Arc} arc
- *
- * @return {[Point[], Number] | Boolean} Point of collision and new line angle if collision exists, else false
- */
-function checkCollision(lineSegmentPoints, arc) {
-    const line = Line.fromPoints(...lineSegmentPoints);
-
-    const intersections = findIntersection(line, arc);
-    if (intersections.length === 0) {
-        return null;
-    }
-    const collisions = intersections.map(function (intersectionPoint) {
-        if (!line.segmentContains(intersectionPoint)) {
-            console.log(line);
-            return;
-        }
-        if (!arc.contains(intersectionPoint)) {
-            return;
-        }
-        return intersectionPoint;
-    });
-
-    const collision = collisions.filter(Boolean)[0]; // There can't be more than 1 collision in our circumstances
-    if (!collision) {
-        return false;
-    }
-    console.log(line.getSlope());
-
-    const relativeCollision = arc.centrate(collision);
-    const angle = Math.atan2(relativeCollision.y, relativeCollision.x);
-
-    const reflectionAngle = angle + (angle - line.getSlope());//TODO: check whether this is right
-    return [collision, reflectionAngle];
-}
-
-// let points = [new Point(0, 2), new Point(3, -1), new Point (1, -1)];
-// let points2 = [new Point(1, -2), new Point(1, 2), new Point (3, 0)];
-// const arc = Arc.fromPoints(...points2);
-// const line = Line.fromPoints(...points.slice(0,2));
-//
-// console.log(arc);
-// console.log(line);
-// console.log(findIntersection(line, arc));
-// console.log(checkCollision(points.slice(0,2), arc));
 
 module.exports = {
     Point,
     Line,
     Circle,
-    Arc,
-    findIntersection,
-    checkCollision
+    Arc
 };
