7c7000438be4bd335aa327b7c24d509bec0bfecd
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-11-07 00:10:22 +0300

    Actual merge

--- src/views/game/View.js
+++ src/views/game/View.js
@@ -1,76 +1,44 @@
-const PIXI = require('pixi.js');
-
 const View = require('../View.js');
-const TemplatedViewMixin = require('../_view_mixins/TemplatedViewMixin.js');
-const RouterLinksViewMixin = require('../_view_mixins/RouterLinksViewMixin.js');
-const template = require('./template.pug');
 const GameManager = require('../../game/GameManager.js');
+const Constants = require('../../utils/Constants.js');
 
 
 class GameView extends View {
-    open() {
-        const gameField = document.getElementById('game');
-
-        let appWidth = window.innerWidth * 0.8;
-        let appHeight = Math.round(appWidth * 9.0 / 16.0);
+    open(root) {
+        let appWidth = window.innerWidth * Constants.WINDOW_SCALE;
+        let appHeight = Math.round(appWidth * Constants.WINDOW_HD_RATIO_INVERSE);
         if (appHeight > window.innerHeight) {
-            appHeight = window.innerHeight * 0.8;
-            appWidth = Math.round(appHeight * 16.0 / 9.0);
+            appHeight = window.innerHeight * Constants.WINDOW_SCALE;
+            appWidth = Math.round(appHeight * Constants.WINDOW_HD_RATIO);
         }
 
-        const gameManager = new GameManager;
+        root.innerHTML = '';
+
+        const gameFieldWrapper = document.createElement('div');
+        gameFieldWrapper.style.width = '100%';
+        gameFieldWrapper.style.height = '100%';
+        gameFieldWrapper.style.backgroundColor = 'black';
+        root.appendChild(gameFieldWrapper);
+
+        const gameField = document.createElement('div');
+        gameField.style.position = 'absolute';
+        gameField.style.top = '50%';
+        gameField.style.marginTop = (-appHeight / 2) + 'px';
+        gameField.style.width = appWidth + 'px';
+        gameField.style.height = appHeight + 'px';
+        gameFieldWrapper.appendChild(gameField);
+
+        const gameManager = new GameManager();
 
         gameManager.setGameField(gameField);
         gameManager.setResolution([appWidth, appHeight]);
 
         gameManager.initiateGame();
-        const app = gameManager.app;
-        console.log(window.innerWidth, window.innerHeight);
-        // Scale mode for all textures, will not retain pixelation
-        const sprite = PIXI.Sprite.fromImage('./images/spacestation.png');
-
-// Set the initial position
-        sprite.anchor.set(0.5);
-        sprite.x = this._getCenterX(app);
-        sprite.y = this._getCenterY(app);
-
-        sprite.height = sprite.width = app.renderer.width / 10;
-
-// Opt-in to interactivity
-        sprite.interactive = true;
-
-// Shows hand cursor
-        sprite.buttonMode = true;
-
-// Pointers normalize touch and mouse
-//     console.error(EventBus.proxy('pointerdown', onClick, sprite));
-        console.error(sprite.on('pointerdown', this.serviceLocator.eventBus.proxy(this._onSpriteClick, sprite)));
-
-// Alternatively, use the mouse & touch events:
-// sprite.on('click', onClick); // mouse-only
-
-        app.stage.addChild(sprite);
     }
 
-    get template() {
-        return template;
-    }
-
-    _onSpriteClick(sprite) {
-        // console.log(args);
-        // console.log(this);
-        sprite.scale.x *= 1.25;
-        sprite.scale.y *= 1.25;
-        sprite.rotation += 0.1;
-    }
-
-    _getCenterX(app) {
-        return app.renderer.width / 2;
-    }
-
-    _getCenterY(app) {
-        return app.renderer.height / 2;
+    close() {
+        // TODO
     }
 }
 
-module.exports = TemplatedViewMixin(RouterLinksViewMixin(GameView));
+module.exports = GameView;
