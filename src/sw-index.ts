7062f32eeb8fb7d834fe702ad6b8230884747d8a
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-11-21 12:14:30 +0300

    Cache assets

---
+++ src/sw-index.ts
@@ -0,0 +1,55 @@
+const images = [
+    '404.png',
+    'background.png',
+    'bounder.png',
+    'energy_block.png',
+    'energy_block_gui_cyan.png',
+    'energy_block_gui_red.png',
+    'energy_block_large.png',
+    'game_over_splash_lost.png',
+    'game_over_splash_won.png',
+    'glitchless.jpg',
+    'laser.png',
+    'laser_large.png',
+    'laser_small.png',
+    'photo_2017-11-07_22-25-34.jpg',
+    'platform.png',
+    'rhythm_blast_v1a.png',
+    'shield_cyan.png',
+    'shield_gui_background.png',
+    'shield_gui_status.png',
+    'shield_red.png',
+    'spacestation.png',
+].map((it) => '/images/' + it);
+
+const cacheName = 'v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(images);
+        }),
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (!response) {
+                return _downloadAssetAndSaveToCache(event);
+            }
+            return response;
+        }),
+    );
+});
+
+function _downloadAssetAndSaveToCache(event) {
+    return fetch(event.request).then((response) => {
+        caches.open(cacheName).then((cache) => {
+            cache.put(event.request, response.clone());
+        });
+        return response;
+    }).catch(() => {
+        return caches.match('/images/404.png');
+    });
+}
