17055629952e6eb6c09b5f70ecb9a2e3e9dbf973
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-11-28 12:18:54 +0300

    Minify, zopfli compression, disable service worker on dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,40 @@
 const gulp = require('gulp');
-const path = require('path');
-const webpack = require('webpack-stream');
-const gulpSass = require('gulp-sass');
+const runSequence = require('run-sequence');
+const del = require('del');
+
 const gulpRename = require('gulp-rename');
 const gulpSourcemaps = require('gulp-sourcemaps');
 const gulpPlumber = require('gulp-plumber');
 const gulpInsert = require('gulp-insert');
 const gulpIf = require('gulp-if');
+
+const webpack = require('webpack-stream');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ZopfliPlugin = require('zopfli-webpack-plugin');
+
+const gulpSass = require('gulp-sass');
 const gulpAutoprefixer = require('gulp-autoprefixer');
+const gulpCleanCSS = require('gulp-clean-css');
+
+const gulpHtmlmin = require('gulp-htmlmin');
+
+const gulpZopfli = require('gulp-zopfli');
+const gulpSize = require('gulp-size');
 
 
 // js
+
+const useStrictCode = `
+    "use strict";
+`;
+
+const removeServiceWorkerCode = `
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for (let registration of registrations) {
+            registration.unregister();
+        } 
+    });
+`;
 
 const webpackConfig = {
     module: {
@@ -42,35 +66,79 @@ const webpackConfig = {
     }
 };
 
-const jsBuildPipe = (inputFilepath, outputFilename, doWatch) => {
+const jsPipe = (inputFilepath, outputFilename, {doWatch = false, doMinify = false, doRemoveServiceWorker = false} = {}) => {
+    const configChanges = {};
+    configChanges.output = {
+        filename: outputFilename
+    };
+    configChanges.watch = doWatch;
+    configChanges.plugins = [];
+    if (doMinify) {
+        configChanges.plugins.push(new UglifyJsPlugin({sourceMap: true}));
+        configChanges.plugins.push(new ZopfliPlugin());
+    }
+
     return gulp.src(inputFilepath)
-        .pipe(webpack(Object.assign({}, webpackConfig, {watch: doWatch, output: {filename: outputFilename}})))
-        .pipe(gulpIf((file) => file.path.endsWith('.js'), gulpInsert.prepend('"use strict";\n')))
+        .pipe(webpack(Object.assign({}, webpackConfig, configChanges)))
+        .pipe(gulpIf((file) => file.path.endsWith('.js'),
+            gulpInsert.prepend(useStrictCode)))
+        .pipe(gulpIf((file) => file.path.endsWith('.js') && doRemoveServiceWorker,
+            gulpInsert.prepend(removeServiceWorkerCode)))
         .pipe(gulp.dest('dist/'));
 };
 
 gulp.task('js:build', () => {
-    return jsBuildPipe('src/index.ts', 'app.js', false);
+    return jsPipe('src/index.ts', 'app.js', {doMinify: true});
 });
 
 gulp.task('js:watch', () => {
-    return jsBuildPipe('src/index.ts', 'app.js', true);
+    return jsPipe('src/index.ts', 'app.js', {doWatch: true});
+});
+
+gulp.task('js:watch-dev', () => {
+    return jsPipe('src/index.ts', 'app.js', {doWatch: true, doRemoveServiceWorker: true});
 });
 
 gulp.task('sw-js:build', () => {
-    return jsBuildPipe('src/sw-index.ts', 'app.js', false);
+    return jsPipe('src/sw-index.ts', 'sw.js', {doMinify: true});
 });
 
 gulp.task('sw-js:watch', () => {
-    return jsBuildPipe('src/sw-index.ts', 'sw.js', true);
+    return jsPipe('src/sw-index.ts', 'sw.js', {doWatch: true});
 });
 
 
 // css
 
+const cssPipe = (p, {doMinify = false} = {}) => {
+    p = p
+        .pipe(gulpSourcemaps.init())
+        .pipe(gulpSass())
+        .pipe(gulpAutoprefixer());
+    if (doMinify) {
+        p = p
+            .pipe(gulpCleanCSS());
+    }
+
+    p = p
+        .pipe(gulpRename('app.css'))
+        .pipe(gulpSourcemaps.write('.'))
+        .pipe(gulpSize({title: 'CSS', showFiles: true, showTotal: false}))
+        .pipe(gulp.dest('dist'));
+
+    if (doMinify) {
+        p = p
+            .pipe(gulpZopfli())
+            .pipe(gulpSize({title: 'CSS gz', showFiles: true, showTotal: false}))
+            .pipe(gulp.dest('dist'));
+    }
+
+    return p;
+};
+
 gulp.task('css:build', () => {
     const p = gulp.src('src/views/index.scss');
-    return cssPipe(p);
+    return cssPipe(p, {doMinify: true});
 });
 
 gulp.task('css:build-dev', () => {
@@ -78,16 +146,6 @@ gulp.task('css:build-dev', () => {
         .pipe(gulpPlumber());
     return cssPipe(p);
 });
-
-const cssPipe = (p) => {
-    return p
-        .pipe(gulpSourcemaps.init())
-        .pipe(gulpSass())
-        .pipe(gulpAutoprefixer())
-        .pipe(gulpRename('app.css'))
-        .pipe(gulpSourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
-};
 
 gulp.task('css:watch', () => {
     return gulp.watch('src/views/**/*.scss', ['css:build-dev']);
@@ -98,19 +156,28 @@ gulp.task('css:watch', () => {
 
 gulp.task('html:build', () => {
     return gulp.src('src/views/index.html')
+        .pipe(gulpHtmlmin({collapseWhitespace: true}))
+        .pipe(gulpSize({title: 'HTML', showFiles: true, showTotal: false}))
+        .pipe(gulp.dest('dist/'))
+        .pipe(gulpZopfli())
+        .pipe(gulpSize({title: 'HTML gz', showFiles: true, showTotal: false}))
         .pipe(gulp.dest('dist/'));
 });
 
 
-// other files
+// images
 
-gulp.task('other:build', () => {
+gulp.task('images:build', () => {
     return gulp.src('src/**/*.{png,jpg,gif}')
+        .pipe(gulpSize({title: 'Images'}))
+        .pipe(gulp.dest('dist/'))
+        .pipe(gulpZopfli())
+        .pipe(gulpSize({title: 'Images gz'}))
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('other:watch', () => {
-    return gulp.watch('src/**/*.{png,jpg,gif}', ['other:build']);
+gulp.task('images:watch', () => {
+    return gulp.watch('src/**/*.{png,jpg,gif}', ['images:build']);
 });
 
 
@@ -141,11 +208,21 @@ gulp.task('test-html:watch', () => {
 });
 
 
+// utility
+
+gulp.task('clean', () => {
+    return del(['dist']);
+});
+
+
 // main
 
-gulp.task('build', ['js:build', 'sw-js:build', 'css:build', 'html:build', 'other:build']);
-gulp.task('watch', ['js:watch', 'sw-js:watch', 'css:watch', 'css:build-dev', 'html:build', 'other:watch', 'other:build']);
+gulp.task('build',
+    (cb) => runSequence('clean', ['js:build', 'sw-js:build', 'css:build', 'html:build', 'images:build'], cb));
+gulp.task('watch',
+    (cb) => runSequence('clean', ['js:watch-dev', 'css:watch', 'css:build-dev', 'html:build', 'images:watch', 'images:build'], cb));
 
-gulp.task('test', ['test-js:watch', 'test-html:watch', 'test-html:build-dev']);
+gulp.task('test',
+    ['test-js:watch', 'test-html:watch', 'test-html:build-dev']);
 
 gulp.task('default', ['build']);
