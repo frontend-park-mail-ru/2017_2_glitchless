9ceb798c3ff503b170b42bec491a2106f96e820b
Oleg Morozenkov <m@oleg.rocks>
2017-12-15 17:51:58 +0300

    Lint

--- src/initializers/authUser.js
+++ src/initializers/authUser.js
@@ -1,6 +1,6 @@
 import UserModel from '../models/UserModel';
 
-export default function (serviceLocator) {
+export default function(serviceLocator) {
     UserModel.loadCurrent(true, serviceLocator).then((user) => {
         serviceLocator.user = user;
         serviceLocator.eventBus.emitEvent('auth', user);
